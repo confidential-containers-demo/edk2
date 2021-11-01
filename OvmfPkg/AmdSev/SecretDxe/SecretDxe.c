c603a85d5b1115283ffa78d39ee8fb1f93822818
Dov Murik <dovmurik@linux.ibm.com>
2021-11-01 11:49:33 +0000

    OvmfPkg/AmdSev: Erase secret area content on ExitBootServices
    The confidential computing secrets area is marked as EfiBootServicesData
    region, which means it is released for the OS use when the OS EFI stub
    calls ExitBootServices.  However, its content is not erased, and
    therefore the OS might unintentionally reuse this sensitive memory area
    and expose the injected secrets.
    
    Erase the content of the secret area on ExitBootServices so that the
    memory released to the OS contains zeros.  If the OS needs to keep the
    secrets for its own use, it must copy the secrets area to another memory
    area before calling ExitBootServices (for example in efi/libstub in
    Linux).
    
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Brijesh Singh <brijesh.singh@amd.com>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Min Xu <min.m.xu@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Tobin Feldman-Fitzthum <tobin@linux.ibm.com>
    Signed-off-by: Dov Murik <dovmurik@linux.ibm.com>

--- OvmfPkg/AmdSev/SecretDxe/SecretDxe.c
+++ OvmfPkg/AmdSev/SecretDxe/SecretDxe.c
@@ -5,6 +5,8 @@
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 #include <PiDxe.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Guid/ConfidentialComputingSecret.h>
 
@@ -13,6 +15,35 @@ STATIC CONFIDENTIAL_COMPUTING_SECRET_LOCATION mSecretDxeTable = {
   FixedPcdGet32 (PcdSevLaunchSecretSize),
 };
 
+STATIC EFI_EVENT mSecretDxeExitBootEvent;
+
+/**
+  ExitBootServices event notification function for the secret table.
+
+  This function erases the content of the secret area so the secrets don't leak
+  via released BootServices memory.  If the OS wants to keep the secrets for
+  its own use, it must copy the secrets area to another memory area before
+  calling ExitBootServices (for example in efi/libstub in Linux).
+
+  @param[in] Event         The ExitBoot event that has been signaled.
+
+  @param[in] Context       Unused.
+**/
+STATIC
+VOID
+EFIAPI
+SecretDxeExitBoot (
+  IN EFI_EVENT Event,
+  IN VOID      *Context
+  )
+{
+  ASSERT(mSecretDxeTable.Base != 0);
+  ASSERT(mSecretDxeTable.Size > 0);
+
+  ZeroMem ((VOID *) ((UINTN) mSecretDxeTable.Base), mSecretDxeTable.Size);
+}
+
+
 EFI_STATUS
 EFIAPI
 InitializeSecretDxe(
@@ -20,8 +51,16 @@ InitializeSecretDxe(
   IN EFI_SYSTEM_TABLE     *SystemTable
   )
 {
-  return gBS->InstallConfigurationTable (
-                &gConfidentialComputingSecretGuid,
-                &mSecretDxeTable
-                );
+  EFI_STATUS Status;
+
+  Status = gBS->InstallConfigurationTable (
+                  &gConfidentialComputingSecretGuid,
+                  &mSecretDxeTable
+                  );
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  return gBS->CreateEvent (EVT_SIGNAL_EXIT_BOOT_SERVICES, TPL_CALLBACK,
+                SecretDxeExitBoot, NULL, &mSecretDxeExitBootEvent);
 }
